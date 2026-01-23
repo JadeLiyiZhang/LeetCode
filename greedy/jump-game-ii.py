bd99bbc7531da24a4c6a63fc7d37af49298854ac
Liyi <jadenzhangliyi@gmail.com>
2026-01-22 23:19:31 -0800

    LC-jump-game-ii [Runtime: 7 ms; Memory: 20.3 MB]

--- greedy/jump-game-ii.py
+++ greedy/jump-game-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        far = 0
-        step = 1
-        for i in range(len(nums)):
-            if far <= i:
-                far = max(far, i + nums[i])
-                if far >= len(nums):
-                    return step
-            else:
+        farthest = 0
+        step = 0
+        end = 0
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            
+            if i == end:
                 step += 1
+                end = farthest
         return step
