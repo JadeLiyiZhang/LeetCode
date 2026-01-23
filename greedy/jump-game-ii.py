65b1023173561c2982bb09efdd27897fa240c7a7
Liyi <jadenzhangliyi@gmail.com>
2026-01-22 23:08:18 -0800

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- greedy/jump-game-ii.py
+++ greedy/jump-game-ii.py
@@ -5,6 +5,8 @@ class Solution:
         for i in range(len(nums)):
             if far <= i:
                 far = max(far, i + nums[i])
+                if far >= len(nums):
+                    return step
             else:
                 step += 1
         return step
