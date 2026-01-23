d39abda7e466899a6a279424fd774aea1017ff55
Liyi <jadenzhangliyi@gmail.com>
2026-01-22 23:07:00 -0800

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- array/jump-game-ii.py
+++ array/jump-game-ii.py
@@ -1,12 +1,10 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        ans = 0
-        cur_end = 0
-        cur_far = 0
-        n = len(nums)
-        for i in range(n - 1):
-            cur_far = max(cur_far, i + nums[i])
-            if i == cur_end:
-                ans += 1
-                cur_end = cur_far
-        return ans
+        far = 0
+        step = 1
+        for i in range(len(nums)):
+            if far <= i:
+                far = max(far, i + nums[i])
+            else:
+                step += 1
+        return step
