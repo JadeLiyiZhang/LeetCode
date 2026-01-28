fd3033850c81fc176e7e380125f9a5978869591c
Liyi <jadenzhangliyi@gmail.com>
2026-01-27 20:38:37 -0800

    LC-roman-to-integer [Runtime: 1 ms; Memory: 19.4 MB]

--- math/roman-to-integer.py
+++ math/roman-to-integer.py
@@ -1,19 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
+            "I": 1, "V": 5, "X": 10, "L": 50,
+            "C": 100, "D": 500, "M": 1000
         }
 
-        res = values.get(s[-1])
-        for i in range(len(s)-2, -1, -1):
+        res = 0
+        for i in range(len(s) - 1):
             if values[s[i]] < values[s[i + 1]]:
                 res -= values[s[i]]
             else:
                 res += values[s[i]]
-        return res
+        return res + values[s[-1]]
