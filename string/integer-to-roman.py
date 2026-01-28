3aadb8940d7125d2dbf3d09c4c7139aa997b4e6d
Liyi <jadenzhangliyi@gmail.com>
2026-01-27 21:09:54 -0800

    LC-integer-to-roman [Runtime: 7 ms; Memory: 19.3 MB]

--- string/integer-to-roman.py
+++ string/integer-to-roman.py
@@ -1,14 +1,11 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        arr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-        str_arr = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-        res = []
-
-        for i in range(len(arr)):
-            if num == 0:
-                break
-
-            times = num // arr[i]
-            res.extend([str_arr[i]] * times)
-            num = num % arr[i]
-        return ''.join(res)
+        pairs = [(1000, 'M'), (900, "CM"), (500, 'D'), (400, 'CD'),
+                    (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+                    (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+        res = ''
+        for val, letter in pairs:
+            while num >= val:
+                num -= val
+                res += letter      
+        return res
