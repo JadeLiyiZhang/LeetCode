664088c36967431aa5ba3093a9153d596ee2819a
Liyi <jadenzhangliyi@gmail.com>
2026-01-27 20:13:13 -0800

    LC-gas-station [Runtime: 23 ms; Memory: 26 MB]

--- greedy/gas-station.py
+++ greedy/gas-station.py
@@ -2,13 +2,15 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-                
-        curernt_gas = 0
+        
+        n = len(gas)
         start = 0
-        for i in range(len(gas)):
-            curernt_gas += gas[i] - cost[i]
-            if curernt_gas < 0:
-                curernt_gas = 0
+        oil = 0
+        for i in range(n):
+            if oil + gas[i] < cost[i]:
                 start = i + 1
-
+                oil = 0
+            else:
+                oil = oil + gas[i] - cost[i]
         return start
+        
