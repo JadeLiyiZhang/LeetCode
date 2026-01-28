46b1f5c2999faf01f6828e3aeef24c7d7f6184aa
Liyi <jadenzhangliyi@gmail.com>
2026-01-27 21:20:03 -0800

    LC-length-of-last-word [Runtime: 0 ms; Memory: 19.4 MB]

--- string/length-of-last-word.py
+++ string/length-of-last-word.py
@@ -1,12 +1,3 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        count = 0
-        for i in range(len(s) - 1, -1, -1):
-            if s[i] == " ":
-                if count == 0:
-                    continue
-                else:
-                    return count
-            else:
-                count += 1
-        return count
+        return len(s.strip().split()[-1])
