0c9ad64985579f8bbee91b7ac84c3e20bfbcf0b7
Liyi <jadenzhangliyi@gmail.com>
2026-02-04 16:06:23 -0800

    LC-zigzag-conversion [Runtime: 34 ms; Memory: 19.6 MB]

--- string/zigzag-conversion.py
+++ string/zigzag-conversion.py
@@ -1,9 +1,12 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
         direction = 1
         res = [[] for _ in range(numRows)]
         row = 0
         for char in s:
+            print(row)
             res[row].append(char)
             if row == 0:
                 direction = 1
