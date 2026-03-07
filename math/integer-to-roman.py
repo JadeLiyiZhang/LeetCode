bf38c987aa24dd4dc9b7abb3e8f6ebd2dd5d6d18
Liyi <jadenzhangliyi@gmail.com>
2026-03-06 23:22:25 -0800

    LC-integer-to-roman [Runtime: 3 ms; Memory: 19.1 MB]

--- math/integer-to-roman.py
+++ math/integer-to-roman.py
@@ -2,9 +2,9 @@ class Solution:
     def intToRoman(self, num: int) -> str:
         table = [(1000, "M"), (900, "CM"), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
         res = ''
-        for int_, roman in table:
-            if num >= int_:
-                count, mod = divmod(num, int_)
+        for value, roman in table:
+            if num >= value:
+                count = num // value
                 res += roman * count 
-                num = mod
+                num = num % value
         return res
