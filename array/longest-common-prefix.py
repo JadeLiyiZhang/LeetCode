e9e513986be8d4b336931a30bc4b41bc3eb9a71b
Liyi <jadenzhangliyi@gmail.com>
2026-03-06 23:59:42 -0800

    LC-longest-common-prefix [Runtime: 3 ms; Memory: 19.2 MB]

--- array/longest-common-prefix.py
+++ array/longest-common-prefix.py
@@ -1,9 +1,10 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix = strs[0]
-        for s in strs[1:]:
-            while not s.startswith(prefix):
-                prefix = prefix[:-1]
-                if prefix == '':
-                    return ''
-        return prefix
+        strs.sort()
+        res = ''
+        for i in range(len(strs[0])):
+            for j in range(len(strs)):
+                if strs[j][i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
