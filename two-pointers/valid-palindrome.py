8bb9f92952d75541d93f70ea6a793eb9ecf376d1
Liyi <jadenzhangliyi@gmail.com>
2026-03-09 00:25:37 -0700

    LC-valid-palindrome [Runtime: 6 ms; Memory: 19.6 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -1,18 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if not s:
-            return True
         s = s.lower()
         left, right = 0, len(s) - 1
-        def isNonAlpha(char):
-            return not (char.isdigit() or char.isalpha())
+        
         while left < right:
-            while left < right and isNonAlpha(s[left]):
+            while left < right and not s[left].isalnum():
                 left += 1
-            while left < right and isNonAlpha(s[right]):
+            while left < right and not s[right].isalnum():
                 right -= 1
             if s[left] != s[right]:
                 return False
+            
             left += 1
             right -= 1
+        
         return True
