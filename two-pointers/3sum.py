4e25bd8e322824a9b6e604df1e03cdcf7575847f
Liyi <jadenzhangliyi@gmail.com>
2026-03-09 15:17:21 -0700

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -1,27 +1,22 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # nums[i] + nums[j] = -nums[k]
-        n = len(nums)
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
         res = []
-        for i in range(len(nums)):
-            if nums[i] > 0:
+        for i in range(en(nums) - 2):
+            if i > 0:
                 break
-            if i > 0 and nums[i] == nums[i - 1]:
+            if nums[i] == nums[i - 1]:
                 continue
             target = -nums[i]
-            left, right = i + 1, n - 1
-            while left < right:
-                if nums[left] + nums[right] == target:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left + 1] == nums[left]:
-                        left += 1
-                    while left < right and nums[right - 1] == nums[right]:
-                        right -= 1
+            left, right = i + 1, len(nums) - 1
+            if nums[left] + nums[right] == target:
+                res.append([nums[i], nums[left], nums[right]])
+            if nums[left] + nums[right] < target:
+                left += 1
+                while nums[left] == nums[left - 1]:
                     left += 1
+            else:
+                right -= 1
+                while nums[right] == nums[right + 1]:
                     right -= 1
-                elif nums[left] + nums[right] > target:
-                    right -= 1
-                else:
-                    left += 1
-        return res
+        return res            
