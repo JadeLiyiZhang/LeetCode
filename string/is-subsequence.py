23597ecf9716444cd3717a259fad934cef5008fe
Liyi <jadenzhangliyi@gmail.com>
2026-03-09 14:53:54 -0700

    LC-is-subsequence [Runtime: 0 ms; Memory: 19.3 MB]

--- string/is-subsequence.py
+++ string/is-subsequence.py
@@ -1,6 +1,6 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if not s or not t:
+        if not s:
             return True
         pointer = 0
         for char in t:
