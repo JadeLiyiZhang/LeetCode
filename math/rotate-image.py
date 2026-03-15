d738771b67d9755de6ac1f067a523b649b4c3744
Liyi <jadenzhangliyi@gmail.com>
2026-03-14 20:42:48 -0700

    LC-rotate-image [Runtime: 0 ms; Memory: 19.3 MB]

--- math/rotate-image.py
+++ math/rotate-image.py
@@ -4,13 +4,13 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix[0])
-        # 转对角线
+        # Transpose the matrix (swap matrix[i][j] with matrix[j][i])
         for i in range(n):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         print(matrix)
 
-        # 左右翻转
+        # Reverse each row (swap matrix[i][j] with matrix[i][n - j - 1])
         for row in matrix:
             row.reverse()
         return matrix
