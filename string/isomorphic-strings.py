1d542236a93a897b01ca93576a4d89ddbc5a4cf7
Liyi <jadenzhangliyi@gmail.com>
2026-03-24 23:05:35 -0700

    LC-isomorphic-strings [Runtime: 3 ms; Memory: 19.2 MB]

--- string/isomorphic-strings.py
+++ string/isomorphic-strings.py
@@ -1,11 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        mapping_s_t = {}
-        mapping_t_s = {}
+        mapping_s_to_t = {}
+        mapping_t_to_s = {}
         for char1, char2 in zip(s, t):
-            if (char1 not in mapping_s_t) and (char2 not in mapping_t_s):
-                mapping_s_t[char1] = char2
-                mapping_t_s[char2] = char1
-            elif (mapping_s_t.get(char1, 0) != char2) or (mapping_t_s.get(char2, 0) != char1):
-                return False
+            # store the mapping in both dic
+            if char1 not in mapping_s_to_t and char2 not in mapping_t_to_s:
+                mapping_s_to_t[char1] = char2
+                mapping_t_to_s[char2] = char1
+            else:
+                if mapping_s_to_t.get(char1, 0) != char2 or mapping_t_to_s.get(char2, 0) != char1:
+                    return False
         return True
