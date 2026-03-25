c405576812ff112b5759934435829c3c98b30441
Liyi <jadenzhangliyi@gmail.com>
2026-03-24 23:12:30 -0700

    LC-word-pattern [Runtime: N/A; Memory: N/A]

--- hash-table/word-pattern.py
+++ hash-table/word-pattern.py
@@ -1,18 +1,14 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        map_pattern_to_s = {}
-        map_s_to_pattern = {}
-        list_s = s.split(" ")
-        if len(pattern) != len(list_s):
-            return False
-        for char, word in zip(pattern, list_s):
-            if char in map_pattern_to_s:
-                if map_pattern_to_s[char] != word:
+        mapping_pattern_to_s = {}
+        mapping_s_to_pattern = {}
+        s = s.split()
+        for char, word in zip(pattern, s):
+            print(char, word)
+            if char not in mapping_pattern_to_s and word not in mapping_s_to_pattern:
+                mapping_pattern_to_s[char] = word
+                mapping_s_to_pattern[word] = char
+            else:
+                if mapping_pattern_to_s.get(char, 0) != word or mapping_s_to_pattern.get(word, 0) != char:
                     return False
-            if word in map_s_to_pattern:
-                if map_s_to_pattern[word] != char:
-                    return False
-            if (word not in map_s_to_pattern) and (char not in map_pattern_to_s):
-                map_pattern_to_s[char] = word
-                map_s_to_pattern[word] = char
         return True
