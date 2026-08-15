346b5e22be4e7786896aa0f4b4110ac1c1071dd1
Liyi <jadenzhangliyi@gmail.com>
2026-08-14 22:00:29 -0700

    LC-find-first-and-last-position-of-element-in-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-first-and-last-position-of-element-in-sorted-array.py
+++ binary-search/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,31 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def find_first(nums, target):
+        if not nums:
+            return [-1, -1]
+        def findFirst(nums, target):
             left, right = 0, len(nums) - 1
-            res = -1
-            while left <= right:
+            while left < right:
                 mid = left + (right - left) // 2
                 if nums[mid] == target:
-                    res = mid
-                    right = mid - 1
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
+                    right = mid
+                elif nums[mid] < target:
                     left = mid + 1
-            return res
+                else:
+                    right = mid - 1
+            return left if nums[right] == target else -1
         
-        def find_last(nums, target):
+        def findLast(nums, target):
             left, right = 0, len(nums) - 1
-            res = -1
-            while left <= right:
-                mid = left + (right - left) // 2
+            while left < right:
+                mid = left + (right - left + 1) // 2
                 if nums[mid] == target:
-                    res = mid
+                    left = mid
+                elif nums[mid] < target:
                     left = mid + 1
-                elif nums[mid] > target:
-                    right = mid - 1
                 else:
-                    left = mid + 1
-            return res
+                    right = mid - 1
+            return left if nums[left] == target else -1
         
-        first = find_first(nums, target)
-        last = find_last(nums, target)
+        first = findFirst(nums, target)
+        last = findLast(nums, target)
         return [first, last]
