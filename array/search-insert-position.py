136ea88dcb1f34a5b8473115d51143bb92fce914
Liyi <jadenzhangliyi@gmail.com>
2026-08-14 22:14:33 -0700

    LC-search-insert-position [Runtime: 39 ms; Memory: 20 MB]

--- array/search-insert-position.py
+++ array/search-insert-position.py
@@ -7,6 +7,6 @@ class Solution:
                 return mid
             if nums[mid] < target:
                 left = mid + 1
-            if nums[mid] > target:
+            else:
                 right = mid - 1
         return left
