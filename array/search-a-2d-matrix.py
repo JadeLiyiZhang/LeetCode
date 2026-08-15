976c85e7b5b1c56aab29fe0f7381971f9c6664b9
Liyi <jadenzhangliyi@gmail.com>
2026-08-14 23:23:28 -0700

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- array/search-a-2d-matrix.py
+++ array/search-a-2d-matrix.py
@@ -1,23 +1,22 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        R, C = len(matrix), len(matrix[0])
-
-        low, high = 0, R - 1
-        while low <= high:
-            mid = (low + high) // 2
-            if matrix[mid][0] <= target <= matrix[mid][C - 1]:
-                l, r = 0, C - 1
-                while l <= r:
-                    m = (l + r) // 2
-                    if matrix[mid][m] == target:
-                        return True
-                    if matrix[mid][m] < target:
-                        l = m + 1
-                    else:
-                        r = m - 1
-                return False
-            elif target < matrix[mid][0]:
-                high = mid - 1
-            else:  # target > matrix[mid][-1]
-                low = mid + 1
+        row, col = len(matrix), len(matrix[0])
+        left, right = 0, row - 1
+        while left < right:
+            mid = left + (right - left + 1) // 2
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] > target:
+                right = mid - 1
+            else:
+                left = mid
+        l, r = 0, col - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if matrix[left][mid] == target:
+                return True
+            elif matrix[left][mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
         return False
