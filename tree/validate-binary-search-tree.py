5c00799f66b888543bed2f242d5170cce7ac5358
Liyi <jadenzhangliyi@gmail.com>
2026-08-29 23:08:55 -0700

    LC-validate-binary-search-tree [Runtime: N/A; Memory: N/A]

--- tree/validate-binary-search-tree.py
+++ tree/validate-binary-search-tree.py
@@ -6,10 +6,15 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def helper(node, low, high):
+        def validation(node, highest, lowest):
             if not node:
                 return True
-            if not  low < node.val < high:
+            if node.val > highest or node.val < lowest:
                 return False
-            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
-        return helper(root, float('-inf'), float('inf'))
+            else:
+                if node.left:
+                    validation(node.left, node.val, lowest)
+                if node.right:
+                    validation(node.right, highest, node.val)
+            return True
+        return validation(root.left, root.val, float('-inf')) and validation(root.right, float('inf'), root.val)
