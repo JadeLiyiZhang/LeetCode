228cabacd8eb62559c1bb3f03dec84c769f76414
Liyi <jadenzhangliyi@gmail.com>
2026-09-11 21:42:06 -0700

    LC-merge-intervals [Runtime: 8 ms; Memory: 23.3 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -1,10 +1,12 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
         intervals.sort()
         res = []
         cur_end = intervals[0][1]
         cur_start = intervals[0][0]
-        for start, end in intervals:
+        for start, end in intervals[1:]:
             if start <= cur_end:
                 cur_end = max(end, cur_end)
             else:
