398ff1dafaa27f0079ac87c8222ff4011bb4b506
Liyi <jadenzhangliyi@gmail.com>
2026-09-12 21:54:39 -0700

    LC-decode-string [Runtime: N/A; Memory: N/A]

--- recursion/decode-string.py
+++ recursion/decode-string.py
@@ -1,16 +1,18 @@
 class Solution:
     def decodeString(self, s: str) -> str:
+        cur_s = ''
+        cur_num = 0
         stack = []
         for char in s:
-            if char != ']':
-                stack.append(char)
-            else:
-                curr = ''
-                while stack and stack[-1] != '[':
-                    curr = stack.pop() + curr
-                stack.pop()
-                num = ''
-                while stack and stack[-1].isdigit():
-                    num = stack.pop() + num
-                stack.append(curr * int(num))
-        return ''.join(stack)
+            if char.isdigit():
+                cur_num = char * 10 + cur_num
+            if char.isalpha():
+                cur_s += char
+            if char == '[':
+                stack.append((cur_s, cur_num))
+                cur_num = 0
+                cur_s = ''
+            if char == ']':
+                s, num = stack.pop()
+                cur_s = s + cur_s * num
+        return cur_s
