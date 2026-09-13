38779e80e0911afb1681284ebe43804c9e567823
Liyi <jadenzhangliyi@gmail.com>
2026-09-12 21:29:29 -0700

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- directed-acyclic-graph/course-schedule.py
+++ directed-acyclic-graph/course-schedule.py
@@ -2,25 +2,25 @@ from collections import defaultdict
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
         course_seq = defaultdict(list)
         for after, pre in prerequisites:
             course_seq[pre].append(after)
-            indegree[after] += 1
 
-        q = deque()
+        visited = [0] * numCourses
 
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
+        def dfs(course):
+            if visited[course] == 1:
+                return False
+            if visited[course] == 2:
+                return True
+            visited[course] = 1
+            for next_course in course_seq[course]:
+                if not dfs(visited[course]):
+                    return False
+            visited[course] = 2
+            return True
         
-        count = 0
-        while q:
-            cur = q.popleft()
-            count += 1
-            
-            for next_course in course_seq[cur]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    q.append(next_course)
-        return count == numCourses
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
