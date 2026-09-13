e1605f9150ec72f4aa3f9e6e80bb21da0c0c0075
Liyi <jadenzhangliyi@gmail.com>
2026-09-12 21:39:34 -0700

    LC-meeting-rooms-ii [Runtime: N/A; Memory: N/A]

--- array/meeting-rooms-ii.py
+++ array/meeting-rooms-ii.py
@@ -2,12 +2,14 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         if len(intervals) == 1:
             return 1
+        intervals.sort()
         res = 1
         heap = [intervals[0][1]]
         for start, end in intervals[1:]:
-            if end < heap[0]:
+            if start < heap[0]:
                 res += 1
                 heapq.heappush(heap, end)
+                print(heap)
             else:
                 heapq.heappop(heap)
                 heapq.heappush(heap, end)
