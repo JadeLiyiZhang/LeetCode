d4f95f9bae1d451effd1a90eb63ea45b0cab87e5
Liyi <jadenzhangliyi@gmail.com>
2026-09-15 16:08:27 -0700

    LC-subsets [Runtime: 0 ms; Memory: 19.4 MB]

--- backtracking/subsets.py
+++ backtracking/subsets.py
@@ -1,13 +1,14 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def dfs(index, path):
-            if index == len(nums):
+        def dfs(start_index, path):
+            if start_index == len(nums):
                 res.append(path.copy())
                 return
-            dfs(index + 1, path)
-            path.append(nums[index])
-            dfs(index + 1, path)
+            dfs(start_index + 1, path)
+            
+            path.append(nums[start_index])
+            dfs(start_index + 1, path)
             path.pop()
         
         dfs(0, [])
