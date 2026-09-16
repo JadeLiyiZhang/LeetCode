3ce6d5c5e7f91386c579840cd44fcead0bc43abc
Liyi <jadenzhangliyi@gmail.com>
2026-09-15 18:32:03 -0700

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/cheapest-flights-within-k-stops.py
+++ heap-priority-queue/cheapest-flights-within-k-stops.py
@@ -5,6 +5,10 @@ class Solution:
         graph = defaultdict(list)
         for start, end, price in flights:
             graph[start].append((end, price))
+       
+        # dist[m][n] = min cost from src to m when used n steps
+        dist = [[float('inf')] * (k + 1) for _ in range(n)]
+        dist[src][0] = 0
         # heap = [price, curr_stop, step_used]
         heap = [(0, src, 0)]
         while heap:
@@ -12,6 +16,8 @@ class Solution:
 
             if stop == dst:
                 return price
+            if price > dist[stop][step]:
+                continue
             if step <= k:
                 for neighbor, p in graph[stop]:
                     heapq.heappush(heap, (price + p, neighbor, step + 1))
