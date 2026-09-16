ca07bd42706627152c6345685684765e6dae2c26
Liyi <jadenzhangliyi@gmail.com>
2026-09-15 23:32:57 -0700

    LC-fruit-into-baskets [Runtime: 190 ms; Memory: 26.1 MB]

--- hash-table/fruit-into-baskets.py
+++ hash-table/fruit-into-baskets.py
@@ -1,19 +1,18 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
         ans = 0
-        for i in range(len(fruits)):
-            res = 1
-            basket_1 = fruits[i]
-            basket_2 = None
-            for j in range(i + 1, len(fruits)):
-                if fruits[j] == basket_1:
-                    res += 1
-                elif basket_2 is None:
-                    basket_2 = fruits[j]
-                    res += 1
-                elif fruits[j] == basket_2:
-                    res += 1
-                else:
-                    break
-            ans = max(res, ans)
-        return ans
+        count = {}
+        for right in range(len(fruits)):
+            fruit = fruits[right]
+            count[fruit] = count.get(fruit, 0) + 1
+            while len(count) > 2:
+                left_fruit = fruits[left]
+                count[left_fruit] -= 1
+
+                if count[left_fruit] == 0:
+                    del count[left_fruit]
+
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans 
