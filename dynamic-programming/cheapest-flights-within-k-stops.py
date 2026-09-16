b4868b40a9217aee0925994b9c36a018103915b8
Liyi <jadenzhangliyi@gmail.com>
2026-09-15 18:32:56 -0700

    LC-cheapest-flights-within-k-stops [Runtime: N/A; Memory: N/A]

--- dynamic-programming/cheapest-flights-within-k-stops.py
+++ dynamic-programming/cheapest-flights-within-k-stops.py
@@ -1,25 +1,45 @@
+import heapq
 from collections import defaultdict
+from typing import List
 
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(
+        self,
+        n: int,
+        flights: List[List[int]],
+        src: int,
+        dst: int,
+        k: int
+    ) -> int:
         graph = defaultdict(list)
+
         for start, end, price in flights:
             graph[start].append((end, price))
-       
-        # dist[m][n] = min cost from src to m when used n steps
-        dist = [[float('inf')] * (k + 2) for _ in range(n)]
+
+        max_edges = k + 1
+        dist = [[float("inf")] * (max_edges + 1) for _ in range(n)]
         dist[src][0] = 0
-        # heap = [price, curr_stop, step_used]
-        heap = [(0, src, 0)]
+
+        heap = [(0, src, 0)]  # price, city, edges_used
+
         while heap:
-            price, stop, step = heapq.heappop(heap) 
+            price, city, edges_used = heapq.heappop(heap)
 
-            if stop == dst:
+            if city == dst:
                 return price
-            if price > dist[stop][step]:
-                continue
-            if step <= k:
-                for neighbor, p in graph[stop]:
-                    heapq.heappush(heap, (price + p, neighbor, step + 1))
-        return -1
 
+            if price > dist[city][edges_used]:
+                continue
+
+            if edges_used == max_edges:
+                continue
+
+            for nei, p in graph[city]:
+                new_price = price + p
+                new_edges = edges_used + 1
+
+                if new_price < dist[nei][new_edges]:
+                    dist[nei][new_edges] = new_price
+                    heapq.heappush(heap, (new_price, nei, new_edges))
+
+        return -1
