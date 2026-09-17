18db08d890d5960d3f06b6abb8ffc0b423830f39
Liyi <jadenzhangliyi@gmail.com>
2026-09-16 17:19:06 -0700

    LC-gas-station [Runtime: 24 ms; Memory: 26.3 MB]

--- array/gas-station.py
+++ array/gas-station.py
@@ -2,12 +2,14 @@ class Solution:
     def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
         if sum(gas) < sum(cost):
             return -1
-        
-        start = 0
-        remain = 0
+        diff = [0] * len(gas)
         for i in range(len(gas)):
-            if gas[i] + remain < cost[i]:
-                start = i + 1
-            else:
-                remain = gas[i] + remain - cost[i]
-        return start if start < len(gas) else -1
+            diff[i] = gas[i] - cost[i]
+        total = 0
+        start = 0
+        for j in range(len(gas)):
+            total = total + diff[j]
+            if total < 0:
+                start = j + 1
+                total = 0
+        return start
