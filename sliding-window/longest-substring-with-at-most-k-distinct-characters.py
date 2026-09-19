d5402e979da27531e9fd1d848ed5d1981d914251
Liyi <jadenzhangliyi@gmail.com>
2026-09-18 21:12:37 -0700

    LC-longest-substring-with-at-most-k-distinct-characters [Runtime: N/A; Memory: N/A]

--- sliding-window/longest-substring-with-at-most-k-distinct-characters.py
+++ sliding-window/longest-substring-with-at-most-k-distinct-characters.py
@@ -2,6 +2,8 @@ class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
         if k == 0:
             return 0
+        if k == 1:
+            return 1
         map = {}
         left = 0
         count = 0
