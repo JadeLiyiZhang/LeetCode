90a087d2d5035717b5ee9cfd8e163ac95c20067d
Liyi <jadenzhangliyi@gmail.com>
2026-09-18 21:12:20 -0700

    LC-longest-substring-with-at-most-k-distinct-characters [Runtime: N/A; Memory: N/A]

--- hash-table/longest-substring-with-at-most-k-distinct-characters.py
+++ hash-table/longest-substring-with-at-most-k-distinct-characters.py
@@ -1,5 +1,7 @@
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        if k == 0:
+            return 0
         map = {}
         left = 0
         count = 0
