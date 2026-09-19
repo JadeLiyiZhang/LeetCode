1f22292b34beb04beae444f229e5bd2619b13b83
Liyi <jadenzhangliyi@gmail.com>
2026-09-18 20:02:55 -0700

    LC-partition-array-for-maximum-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/partition-array-for-maximum-sum.py
+++ dynamic-programming/partition-array-for-maximum-sum.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: list[int], k: int) -> int:
+        # dp[i]: the max value we can get from 0 - i numbers in the arr after partition
         dp = [0] * (len(arr) + 1)
+        
         for i in range(1, len(arr) + 1):
             max_val = 0
-
+            # length: the length of the last partition
             for length in range(1, min(k, i) + 1):
                 max_val = max(max_val, arr[i - length])
                 dp[i] = max(dp[i], dp[i - length] + max_val * length)
