278c055b9f36fcd8fd264c23d1aaba15779c8bdc
Liyi <jadenzhangliyi@gmail.com>
2026-09-18 20:19:32 -0700

    LC-subarray-sums-divisible-by-k [Runtime: 97 ms; Memory: 23.5 MB]

--- array/subarray-sums-divisible-by-k.py
+++ array/subarray-sums-divisible-by-k.py
@@ -1,9 +1,10 @@
 class Solution:
     def subarraysDivByK(self, nums: list[int], k: int) -> int:
+        prefix_table = {0: 1}
         res = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums) + 1):
-                temp = sum(nums[i:j])
-                if temp % k == 0:
-                    res += 1
+        prefix_sum = 0
+        for num in nums:
+            prefix_sum += num
+            res += prefix_table.get(prefix_sum % k, 0)
+            prefix_table[prefix_sum % k] = prefix_table.get(prefix_sum % k, 0) + 1
         return res
